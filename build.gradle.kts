a2f9e9960b18540d3209753d1989861d82ec13b4
Lilly Tempest <46890129+RainbowDashLabs@users.noreply.github.com>
2022-07-01 19:53:39 +0200

    Switch to stable SBR version

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ dependencies {
         exclude("com.zaxxer")
     }
 
-    compileOnly("de.eldoria", "schematicbrushreborn-api", "2.2.0-DEV")
+    compileOnly("de.eldoria", "schematicbrushreborn-api", "2.2.0")
     compileOnly("org.spigotmc", "spigot-api", "1.16.5-R0.1-SNAPSHOT")
     compileOnly("com.sk89q.worldedit", "worldedit-bukkit", "7.2.10")
 
