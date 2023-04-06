23f327a2984423bb8bf5b8d44971deed5d6bc6de
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2023-04-07 01:24:48 +0200

    Add license header

--- src/main/java/de/eldoria/sbrdatabase/configuration/Configuration.java
+++ src/main/java/de/eldoria/sbrdatabase/configuration/Configuration.java
@@ -1,3 +1,9 @@
+/*
+ *     SPDX-License-Identifier: AGPL-3.0-only
+ *
+ *     Copyright (C) 2021 EldoriaRPG Team and Contributor
+ */
+
 package de.eldoria.sbrdatabase.configuration;
 
 import de.eldoria.sbrdatabase.configuration.elements.Cache;
